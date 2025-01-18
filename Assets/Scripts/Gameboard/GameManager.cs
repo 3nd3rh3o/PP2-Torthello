27104d191ba2978d75621cd05211540f9496391f
enderh3o <theenderman3333@gmail.com>
2025-01-18 14:50:22 +0100

    Added camera controller, and prototype of cursor

--- Assets/Scripts/Gameboard/GameManager.cs
+++ Assets/Scripts/Gameboard/GameManager.cs
@@ -1,9 +1,8 @@
-using NUnit.Framework.Constraints;
 using UnityEngine;
+using UnityEngine.InputSystem;
 
 
 [RequireComponent(typeof(MeshFilter))]
-[ExecuteInEditMode]
 public class GameManager : MonoBehaviour
 {
 
@@ -20,11 +19,25 @@ public class GameManager : MonoBehaviour
     private CombineInstance[] ci;
     private Mesh whiteMesh;
     private Mesh blackMesh;
+    public InputActionReference playerLook;
+    public InputActionReference rightClick;
+
+    private Vector2 mousePos;
+
+    public Vector2 mouseSensitivity = new();
+    public Vector2 camSensitivity = new();
+
+    public GameObject cursor;
+
+    public Vector2 camAngles;
+    private Vector3 camPos;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void OnEnable()
     {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
         whiteMesh = new();
         blackMesh = new();
         ci = new CombineInstance[2];
@@ -38,6 +51,24 @@ public class GameManager : MonoBehaviour
         GetComponent<MeshFilter>().sharedMesh.Clear();
         GetComponent<MeshFilter>().sharedMesh.CombineMeshes(ci, false, true, false);
 
+        camPos = new(0, 0, -10);
+
+        camAngles = new(90, 0);
+
+        Camera.main.transform.position = Quaternion.Euler(0, camAngles.y, 0) * Quaternion.Euler(camAngles.x - 90f, 0, 0) * camPos;
+        Camera.main.transform.rotation = Quaternion.Euler(0, camAngles.y, 0) * Quaternion.Euler(camAngles.x - 90f, 0, 0);
+
+        cursor.transform.rotation = Camera.main.transform.rotation;
+
+
+        cursor.SetActive(true);
+
+    }
+
+
+    void OnDisable()
+    {
+        cursor?.SetActive(false);
     }
 
     // Update is called once per frame
@@ -56,21 +87,33 @@ public class GameManager : MonoBehaviour
 
         GetComponent<MeshFilter>().sharedMesh.Clear();
         GetComponent<MeshFilter>().sharedMesh.CombineMeshes(ci, false, false);
-    }
 
-    void OnDrawGizmos()
-    {
-        //Gizmos.DrawSphere(TorusSpaceHelper.OSToWS(transform, TorusSpaceHelper.IndexToPos(testPointI, testPointJ, numberOfSection * 2, pointsPerSection * 2, radius, sectionRadius)), 0.1f);
-        for (int i = 0; i < numberOfSection * 2; i++)
+        if (playerLook != null)
         {
-            for (int j = 0; j < pointsPerSection * 2; j++)
+            //Mouse mouvement
+            Vector2 mouseDelta = playerLook.action.ReadValue<Vector2>();
+            mousePos.x = Mathf.Max(-1, Mathf.Min(mousePos.x + mouseDelta.x * mouseSensitivity.x, 1));
+            mousePos.y = Mathf.Max(-1, Mathf.Min(mousePos.y + mouseDelta.y * mouseSensitivity.y, 1));
+
+
+
+            if (rightClick != null && rightClick.action.ReadValue<float>() == 1f)
             {
-                Vector3 centerOfTileOS = TorusSpaceHelper.IndexToPos(i, j, numberOfSection * 2, pointsPerSection * 2, radius, sectionRadius);
-                Vector3 normalOfTileOS = TorusSpaceHelper.IndexToTileNormal(i, j, numberOfSection * 2, pointsPerSection * 2, radius, sectionRadius);
-                
-                //Gizmos.DrawLine(TorusSpaceHelper.OSToWS(transform, centerOfTileOS), TorusSpaceHelper.OSToWS(transform, centerOfTileOS + (normalOfTileOS * 0.2f)));
+                //Camera mouvement
+                if (camAngles.x < 180 && mouseDelta.y < 0 || camAngles.x > 0 && mouseDelta.y > 0) camAngles.x += mouseDelta.y * camSensitivity.y;
+                camAngles.y += mouseDelta.x * camSensitivity.x;
+                camAngles.y = camAngles.y % 360;
+
+                Camera.main.transform.position = Quaternion.Euler(0, camAngles.y, 0) * Quaternion.Euler(camAngles.x - 90f, 0, 0) * camPos;
+                Camera.main.transform.rotation = Quaternion.Euler(0, camAngles.y, 0) * Quaternion.Euler(camAngles.x - 90f, 0, 0);
+
+                cursor.transform.rotation = Camera.main.transform.rotation;
             }
+
         }
+
+        cursor.transform.position = MouseHelper.GetLerpedPosOnClipPlaneWS(Camera.main, mousePos) * 0.999f;
+
 
     }
 }
---
+++ Assets/Scripts/Utils/MouseHelper.cs
@@ -0,0 +1,30 @@
+using UnityEngine;
+
+
+
+public class MouseHelper
+{
+    
+
+
+
+
+    public static Vector3[] GetNearClipPlaneWS(Camera camera)
+    {
+        Vector3 t = camera.transform.up * (Mathf.Tan((camera.fieldOfView/2f) * Mathf.Deg2Rad)  * camera.transform.forward * camera.nearClipPlane).magnitude;
+        Vector3 l = -camera.transform.right * ((t.magnitude*2f) * camera.aspect)/2f;
+        Vector3 p = camera.transform.position + (camera.transform.forward * (camera.nearClipPlane + 0.01f));
+        return new Vector3[]{p+l+t, p-l+t, p-l-t, p+l-t};
+    }
+
+    public static Vector3 GetLerpedPosOnClipPlaneWS(Camera camera, Vector2 cursorPos)
+    {
+        Vector3[] WSCorners = GetNearClipPlaneWS(camera);
+        Vector3 a = WSCorners[0];
+        Vector3 b = WSCorners[1];
+        Vector3 d = WSCorners[3];
+        Vector3 x = a + (((b-a)*(cursorPos.x+1))/2f);
+        Vector3 y = (((d-a)*(cursorPos.y+1))/2f);
+        return (x+y);
+    }
+}
