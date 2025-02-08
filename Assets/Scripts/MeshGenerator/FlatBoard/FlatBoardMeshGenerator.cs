fa082e3ce7c4b41dfe0f75f2de4d77283ad6bfa5
EMeziane <98912022+EMeziane@users.noreply.github.com>
2025-02-08 14:58:08 +0100

    added CreateSquareMesh method

--- Assets/Scripts/MeshGenerator/FlatBoard/FlatBoardMeshGenerator.cs
+++ Assets/Scripts/MeshGenerator/FlatBoard/FlatBoardMeshGenerator.cs
@@ -1,4 +1,5 @@
 using UnityEngine;
+using System.Linq;
 
 namespace Tortello
 {
@@ -6,10 +7,19 @@ namespace Tortello
     {
         [Range(4, 20)] public int BoardWidth = 8;
         [Range(4, 20)] public int BoardHeight = 8;
-        
+        [Range(0f,10f)] public float sideLength = 1f;
+        public Vector3 center;
+        private CombineInstance[] combines;
+
         public void InitMesh(MeshFilter meshFilter)
         {
-            throw new System.NotImplementedException();
+            if (meshFilter.sharedMesh == null)
+            {
+                meshFilter.sharedMesh=new();
+            }
+            Mesh mesh = meshFilter.sharedMesh;
+            mesh.Clear();
+
         }
 
         public void UpdateMesh(MeshFilter meshFilter)
@@ -20,6 +30,32 @@ namespace Tortello
         public void Destroy(MeshFilter mF)
         {
             throw new System.NotImplementedException();
+        }
+
+
+
+
+        public static void CreateSquareMesh(Vector3 center, float sideLength, Mesh mesh){
+            Vector3[] points = new Vector3[4];
+            points[0] = center + new Vector3(0.5f*sideLength,0f,0.5f*sideLength);
+            points[1] = center + new Vector3(0.5f*sideLength,0f,-0.5f*sideLength);
+            points[2] = center + new Vector3(-0.5f*sideLength,0f,-0.5f*sideLength);
+            points[3] = center + new Vector3(-0.5f*sideLength,0f,0.5f*sideLength);
+
+            int[] f = new int[2*3];
+            //First triangle
+            f[0]=0;
+            f[1]=1;
+            f[2]=2;
+
+            //Second triangle
+            f[3]=2;
+            f[4]=3;
+            f[5]=0;
+
+            mesh.vertices = points;
+            mesh.triangles = f;
+            mesh.RecalculateNormals();
         }
     }
 }
