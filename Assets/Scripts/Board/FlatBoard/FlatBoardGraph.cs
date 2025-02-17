bc17ae7d9b8cbd092e10f3dc50c5fe06ae1d7a4d
Red-Fang <loicalfonsi24@gmail.com>
2025-02-17 16:29:39 +0100

    Fixed Score Sur

--- Assets/Scripts/Board/FlatBoard/FlatBoardGraph.cs
+++ Assets/Scripts/Board/FlatBoard/FlatBoardGraph.cs
@@ -34,22 +34,13 @@ namespace Tortello
             // on ajoute un pion selement si le coup est valide
             if (CoupEstValide(idSommets, couleur, pionsARetournes))
             {
+
                 if(couleur == Couleur.Noir){
                     pawnNoir++;
                 }
                 else{
                     pawnBlanc++;
                 }
-                if(couleur == Couleur.Noir){
-                    pawnNoir++;
-                }
-                else{
-                    pawnBlanc++;
-                }
-
-                List<int> score = GetScore();
-                Debug.Log("Score Blanc: " + score[0] + " Score Noir: " + score[1]);
-
                 graph.sommets[idSommets].couleur = couleur;
 
                 foreach (List<int> pions in pionsARetournes)
