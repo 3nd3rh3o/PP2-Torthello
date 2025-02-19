b9d5c3e33ba4dbb18b6d343d73fefd0f4345b925
3nd3r_h3o <theenderman3333@gmail.com>
2025-02-19 12:46:36 +0100

    AAAAAAAAAAAAAAAAAAHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHH

--- Assets/UI/UIRoot.cs
+++ Assets/UI/UIRoot.cs
@@ -6,19 +6,40 @@ namespace Tortello
 {
     public class UIRoot : MonoBehaviour
     {
+        public VisualTreeAsset MenuAsset;
+        private TemplateContainer menu;
+        public VisualTreeAsset MenuOptionAsset;
+        private TemplateContainer option;
         private UIDocument uiDocument;
-        private Button button;
-        // action a bind a un boutton.
-        private readonly Action quit = () => Application.Quit();
+        // action a bind aux bouttons
+        private readonly Action menu_quit = () => Application.Quit();
+        private Action menu_option;
+        private Action option_menu;
+
+
+
+
 
         public Settings settings;
         void OnEnable()
         {
-            uiDocument = GetComponent<UIDocument>();
+            menu_option = () =>
+            {
+                EnableOptionFromMenu();
+            };
 
-            VisualElement root = uiDocument.rootVisualElement;
-            // register de l'action au boutton Menu_Exit_Button.
-            root.Q<Button>("Menu_Exit_button").clicked += quit;
+            option_menu = () =>
+            {
+                DisableOption();
+                EnableMenu();
+            };
+
+            uiDocument = GetComponent<UIDocument>();
+            menu = MenuAsset.Instantiate();
+            option = MenuOptionAsset.Instantiate();
+
+
+            EnableMenu();
         }
         void Update()
         {
@@ -33,9 +54,50 @@ namespace Tortello
 
         void OnDisable()
         {
-            VisualElement root = uiDocument.rootVisualElement;
-            //Unregister, comme ça on peut switch les callbackevent sans problème.
-            root.Q<Button>("Menu_Exit_button").clicked -= quit;
+            DisableMenu();
         }
+
+        void EnableMenu()
+        {
+            VisualElement root = uiDocument.rootVisualElement;
+            root.Add(menu);
+
+            // register de l'action au boutton Menu_Exit_Button.
+            root.Q<Button>("Menu_Exit_button").clicked += menu_quit;
+            root.Q<Button>("Menu_Option_button").clicked += menu_option;
+
+        }
+
+        void DisableMenu()
+        {
+            VisualElement root = uiDocument.rootVisualElement;
+
+            //Unregister, comme ça on peut switch les callbackevent sans problème.
+            root.Q<Button>("Menu_Exit_button").clicked -= menu_quit;
+            root.Q<Button>("Menu_Option_button").clicked -= menu_option;
+            // On desactive l'ui du menu
+            root.Remove(menu);
+        }
+
+        void EnableOptionFromMenu()
+        {
+            DisableMenu();
+            VisualElement root = uiDocument.rootVisualElement;
+            root.Add(option);
+
+            root.Q<Button>("Option_Menu_button").clicked += option_menu;
+        }
+
+        void DisableOption()
+        {
+            VisualElement root = uiDocument.rootVisualElement;
+            root.Remove(option);
+
+            //FIXME null error >_<
+            root.Q<Button>("Option_Menu_button").clicked -= option_menu;
+            EnableMenu();
+        }
+
+        
     }
 }
