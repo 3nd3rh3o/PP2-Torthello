3c0c17ce39222a0f7daefda17748d8f7b681e984
enderh3o <theenderman3333@gmail.com>
2025-02-21 19:53:05 +0100

    re completed main loop

--- Assets/Scripts/Board/Board.cs
+++ Assets/Scripts/Board/Board.cs
@@ -85,99 +85,107 @@ namespace Torthello
             if (settings.isInGame)
             {
                 hoveredTile = inputSystem.GetTileHoveredID();
-                if (hoveredTile != -1)
+
+                if (coolDown < 2f) coolDown += Time.deltaTime;
+                if (settings.PlayerNoir == PlayerType.Human && couleur == Couleur.Noir)
                 {
-                    if (coolDown < 2f) coolDown += Time.deltaTime;
-                    if (settings.PlayerNoir == PlayerType.Human && couleur == Couleur.Noir)
+                    coolDown = 0f;
+                    if (inputSystem.Place())
                     {
-                        coolDown = 0f;
-                        if (inputSystem.Place())
+                        List<List<int>> pionRetourne = new();
+                        if (Graph.AddPawn(hoveredTile, couleur, pionRetourne))
                         {
-                            List<List<int>> pionRetourne = new();
-                            if (Graph.AddPawn(hoveredTile, couleur, pionRetourne))
+                            pawnProccessor.SpawnPawn(hoveredTile, couleur);
+                            pawnProccessor.FlipAnimSeq(pionRetourne);
+                            couleur = Couleur.Blanc; // Change le joueur actif à l'IA
+                            settings.turn = "Blanc";
+                            if (Graph.NoPlacementAvailable(couleur))
                             {
-                                pawnProccessor.SpawnPawn(hoveredTile, couleur);
-                                pawnProccessor.FlipAnimSeq(pionRetourne);
-                                couleur = Couleur.Blanc; // Change le joueur actif à l'IA
-                                settings.turn = "Blanc";
-                                if (Graph.NoPlacementAvailable(couleur))
-                                {
-                                    settings.turn = "FINI!";
-                                }
-                            }
-                            else
-                            {
-                                MaterialHandler.FailedPlacement();
+                                settings.turn = "FINI!";
                             }
                         }
-                    }
-                    else if (settings.PlayerNoir == PlayerType.MiniMax && couleur == Couleur.Noir && coolDown > 2f)
-                    {
-                        coolDown = 0f;
-                        int bestMove = aiPlayerNoir.GetBestMove();
-                        if (bestMove != -1)
+                        else
                         {
-                            List<List<int>> pionRetourne = new();
-                            if (Graph.AddPawn(bestMove, couleur, pionRetourne))
-                            {
-                                pawnProccessor.SpawnPawn(bestMove, couleur);
-                                pawnProccessor.FlipAnimSeq(pionRetourne);
-                                couleur = Couleur.Blanc; // Change le joueur actif à l'humain
-                                settings.turn = "Blanc";
-                                if (Graph.NoPlacementAvailable(couleur))
-                                {
-                                    settings.turn = "FINI!";
-                                }
-                            }
+                            MaterialHandler.FailedPlacement();
                         }
                     }
-                    else if (settings.PlayerBlanc == PlayerType.Human && couleur == Couleur.Blanc)
+                }
+                else if (settings.PlayerNoir == PlayerType.MiniMax && couleur == Couleur.Noir && coolDown > 2f)
+                {
+                    coolDown = 0f;
+                    int bestMove = aiPlayerNoir.GetBestMove();
+                    if (bestMove != -1)
                     {
-                        coolDown = 0f;
-                        if (inputSystem.Place())
+                        List<List<int>> pionRetourne = new();
+                        if (Graph.AddPawn(bestMove, couleur, pionRetourne))
                         {
-                            List<List<int>> pionRetourne = new();
-                            if (Graph.AddPawn(hoveredTile, couleur, pionRetourne))
+                            pawnProccessor.SpawnPawn(bestMove, couleur);
+                            pawnProccessor.FlipAnimSeq(pionRetourne);
+                            couleur = Couleur.Blanc; // Change le joueur actif à l'humain
+                            settings.turn = "Blanc";
+                            if (Graph.NoPlacementAvailable(couleur))
                             {
-                                pawnProccessor.SpawnPawn(hoveredTile, couleur);
-                                pawnProccessor.FlipAnimSeq(pionRetourne);
-                                couleur = Couleur.Noir; // Change le joueur actif à l'IA
-                                settings.turn = "Noir";
-                                if (Graph.NoPlacementAvailable(couleur))
-                                {
-                                    settings.turn = "FINI!";
-                                }
-                            }
-                            else
-                            {
-                                MaterialHandler.FailedPlacement();
-                            }
-                        }
-                    }
-                    else if (settings.PlayerBlanc == PlayerType.MiniMax && couleur == Couleur.Blanc && coolDown > 2f)
-                    {
-                        coolDown = 0f;
-                        int bestMove = aiPlayerBlanc.GetBestMove();
-                        if (bestMove != -1)
-                        {
-                            List<List<int>> pionRetourne = new();
-                            if (Graph.AddPawn(bestMove, couleur, pionRetourne))
-                            {
-                                pawnProccessor.SpawnPawn(bestMove, couleur);
-                                pawnProccessor.FlipAnimSeq(pionRetourne);
-                                couleur = Couleur.Noir; // Change le joueur actif à l'humain
-                                settings.turn = "Noir";
-                                if (Graph.NoPlacementAvailable(couleur))
-                                {
-                                    settings.turn = "FINI!";
-                                }
+                                settings.turn = "FINI!";
                             }
                         }
                     }
                 }
+                else if (settings.PlayerBlanc == PlayerType.Human && couleur == Couleur.Blanc)
+                {
+                    coolDown = 0f;
+                    if (inputSystem.Place())
+                    {
+                        List<List<int>> pionRetourne = new();
+                        if (Graph.AddPawn(hoveredTile, couleur, pionRetourne))
+                        {
+                            pawnProccessor.SpawnPawn(hoveredTile, couleur);
+                            pawnProccessor.FlipAnimSeq(pionRetourne);
+                            couleur = Couleur.Noir; // Change le joueur actif à l'IA
+                            settings.turn = "Noir";
+                            if (Graph.NoPlacementAvailable(couleur))
+                            {
+                                settings.turn = "FINI!";
+                            }
+                        }
+                        else
+                        {
+                            MaterialHandler.FailedPlacement();
+                        }
+                    }
+                }
+                else if (settings.PlayerBlanc == PlayerType.MiniMax && couleur == Couleur.Blanc && coolDown > 2f)
+                {
+                    coolDown = 0f;
+                    int bestMove = aiPlayerBlanc.GetBestMove();
+                    if (bestMove != -1)
+                    {
+                        List<List<int>> pionRetourne = new();
+                        if (Graph.AddPawn(bestMove, couleur, pionRetourne))
+                        {
+                            pawnProccessor.SpawnPawn(bestMove, couleur);
+                            pawnProccessor.FlipAnimSeq(pionRetourne);
+                            couleur = Couleur.Noir; // Change le joueur actif à l'humain
+                            settings.turn = "Noir";
+                            if (Graph.NoPlacementAvailable(couleur))
+                            {
+                                settings.turn = "FINI!";
+                            }
+                        }
+                    }
+                }
+
+                MaterialHandler.SetHoveredTile(hoveredTile);
             }
-            MaterialHandler.SetHoveredTile(hoveredTile);
             if (settings.startCMD) StartGame();
+            if (settings.rebuildBoardCMD)
+            {
+                settings.isInGame = false;
+                settings.rebuildBoardCMD = false;
+                coolDown = 0f;
+                Graph.RemoveAllPawns();
+                pawnProccessor.RemoveAllPawns();
+                MaterialHandler.SetHoveredTile(-1);
+            }
             MaterialHandler.UpdateMeshRenderer(mR);
         }
 
