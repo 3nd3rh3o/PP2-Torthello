7035962ca3448121de9cf82b073502f60b83f3bf
Arthur-Bouchery <73992802+Arthur-Bouchery@users.noreply.github.com>
2025-02-24 15:04:43 +0100

    Improved debug messages

--- Assets/Scripts/IA/PlayerMiniMax.cs
+++ Assets/Scripts/IA/PlayerMiniMax.cs
@@ -12,8 +12,8 @@ namespace Torthello
         private IGraph graph;
         private Couleur couleur;
         private int maxDepth = 5; // Profondeur maximale de l'exploration
-
-        public PlayerMiniMax(IGraph graph, Couleur couleur, int maxDepth = 5)
+        private int nbEval = 0; // Nombre d'évaluations effectuées
+        public PlayerMiniMax(IGraph graph, Couleur couleur, int maxDepth = 50)
         {
             this.graph = graph;
             this.couleur = couleur;
@@ -25,28 +25,38 @@ namespace Torthello
             List<int> validMoves = graph.GetValidMoves(couleur);
             int bestMove = -1;
             int bestValue = int.MinValue;
+            nbEval = 0;
 
             foreach (int move in validMoves)
             {
                 List<List<int>> pawnsToFlip = new List<List<int>>();
-                graph.AddPawn(move, couleur, pawnsToFlip);
-                int moveValue = Minimax(graph, maxDepth, false, (couleur == Couleur.Noir) ? Couleur.Blanc : Couleur.Noir, int.MinValue, int.MaxValue);
-                graph.RemovePawn(move, pawnsToFlip); // Remettre le graphe en ordre
-                
-                if (moveValue > bestValue)
+                if(graph.AddPawn(move, couleur, pawnsToFlip))
                 {
-                    bestValue = moveValue;
-                    bestMove = move;
+                    int moveValue = Minimax(graph, maxDepth, false, (couleur == Couleur.Noir) ? Couleur.Blanc : Couleur.Noir, int.MinValue, int.MaxValue);
+                    graph.RemovePawn(move, pawnsToFlip); // Remettre le graphe en ordre
+                    
+                    if (moveValue > bestValue)
+                    {
+                        bestValue = moveValue;
+                        bestMove = move;
+                    }
                 }
+                else {
+                    Debug.Log($"Coup invalide tenté par minimax id: {move} pour le joueur {couleur}");
+                };
             }
-            Debug.Log($"Meilleur coup: {bestMove} avec une valeur de {bestValue}");
+            Debug.Log($"Meilleur coup: {bestMove} avec une valeur de {bestValue} pour le joueur {couleur} avec {nbEval} évaluations");
             return bestMove;
         }
 
         private int Minimax(IGraph graph, int depth, bool isMaximizingPlayer, Couleur playerColor, int alpha, int beta)
         {
-            if (depth <= 0 || graph.NoPlacementAvailable(playerColor))
+           if (depth <= 0 || graph.NoPlacementAvailable(playerColor))
             {
+                if(depth<=0)
+                    Debug.Log($"Fin de la récursion de minimax à la profondeur {maxDepth-depth}");
+                else Debug.Log($"Fin de la récursion de minimax à l'arrêt du jeu");
+                Debug.Log($"Évaluation de la position à la profondeur {maxDepth-depth}, blanc: {graph.GetScore()[0]} noir: {graph.GetScore()[1]}");
                 return Evaluate(graph, playerColor);
             }
             if (isMaximizingPlayer)
@@ -67,7 +77,7 @@ namespace Torthello
                         }
                     }
                     else {
-                        Debug.Log($"Coup invalide tenté par minimax id: {move} profondeur: {depth} pour le joueur {playerColor}");
+                        Debug.Log($"Coup invalide tenté par minimax id: {move} profondeur: {maxDepth-depth} pour le joueur {playerColor}");
                     };
                 }
                 return bestValue;
@@ -90,21 +100,25 @@ namespace Torthello
                         }
                     }
                     else {
-                        Debug.Log($"Coup invalide tenté par minimax id: {move} profondeur: {depth} pour le joueur {playerColor}");
+                        Debug.Log($"Coup invalide tenté par minimax id: {move} profondeur: {maxDepth-depth} pour le joueur {playerColor}");
                     };
                 }
                 return bestValue;
             }
         }
-        private int Evaluate(IGraph graph, Couleur playerColor)
+         private int Evaluate(IGraph graph, Couleur playerColor)
         {
             // Utiliser la méthode GetScore pour évaluer le score
             List<int> scores = graph.GetScore();
             int scoreBlanc = scores[0];
             int scoreNoir = scores[1];
 
+            int score = playerColor == Couleur.Blanc ? scoreBlanc - scoreNoir : scoreNoir - scoreBlanc;
+            //TESTS DEBUG
+            nbEval++;
+            Debug.Log($"Score: {score} Blanc: {scoreBlanc} Noir: {scoreNoir}");
             // Retourner le score en fonction de la couleur du joueur
-            return playerColor == Couleur.Blanc ? scoreBlanc : scoreNoir;
+            return score;
         }
     }
 }
