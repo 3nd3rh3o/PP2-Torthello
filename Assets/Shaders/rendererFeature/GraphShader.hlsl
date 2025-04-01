3fdf533ef5d399515f462bb4b4a842d1b8f1fc56
EMeziane <98912022+EMeziane@users.noreply.github.com>
2025-04-01 17:42:51 +0200

    surface description function

--- Assets/Shaders/rendererFeature/GraphShader.hlsl
+++ Assets/Shaders/rendererFeature/GraphShader.hlsl
@@ -43,7 +43,7 @@ Shader "Tortello/RendererFeature/GraphRender"
                 return 0.;
             }
             // return dist between p and v.
-            float pointDist(float p, float v)
+            float pointDist(float2 p, float2 v)
             {
                 return length(v-p);
             }
@@ -56,6 +56,22 @@ Shader "Tortello/RendererFeature/GraphRender"
                 // [(0. , 0.) -> (1. , 1.)]
                 float2 fragPos = IN.ScreenPosition.xy;
 
+                for(int i = 0; i < _numNodes; i++ ){
+                    if(pointDist(fragPos,_nodes[i].xy) <= _nodesRaduis ){
+                        surface.BaseColor = _nodes[i].z == 0? _nodesColor : _nodes[i].z == 1? float3(0. ,0. ,0.) : float3(1., 1., 1.);
+                        surface.Alpha = 1.;
+                        return surface;
+                    }
+                }
+                for(int i = 0; i < _numEdges; i++){
+                    if( pointDist(fragPos, _nodes[_edges[2*i]].xy) <= _edgesRadius || pointDist(fragPos, _nodes[_edges[2*i+1]].xy) <= _edgesRadius || 
+                    ( projDist(_nodes[_edges[2*i]].xy, _nodes[_edges[2*i+1]].xy, fragPos) <= _edgesRadius && projDist(_nodes[_edges[2*i+1]].xy, _nodes[_edges[2*i]].xy, fragPos) <= _edgesRadius ) ){
+                        surface.BaseColor = _edgesColors[i];
+                        surface.Alpha = 1.;
+                        return surface;
+                    }
+                }
+
                 // for each node, test if in range => draw node.
                 // else if in range of edges (close to end points or (proj in both ways.)) => draw edges.
                 // else color = 0, 0, 0; alpha = 1.
@@ -64,8 +80,8 @@ Shader "Tortello/RendererFeature/GraphRender"
                 
 
 
-                surface.BaseColor = float3(1., 1., 1.);
-                surface.Alpha = 1;
+                surface.BaseColor = float3(0., 0., 0.);
+                surface.Alpha = 0.;
                 return surface;
             }
             
